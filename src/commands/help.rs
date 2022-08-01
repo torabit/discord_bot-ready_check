0e4d3045e13b40103d53c290804225c224703c49
torabit <pujigami@gmail.com>
2022-08-02 00:17:02 +0900

    add help

---
+++ src/commands/help.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+use serenity::framework::standard::{
+    help_commands, macros::help, Args, CommandGroup, CommandResult, HelpOptions,
+};
+
+use serenity::model::{channel::Message, id::UserId};
+use serenity::prelude::Context;
+
+#[help] // Helpコマンド
+#[individual_command_tip = "これはヘルプコマンド"] // Helpコマンドの説明
+#[strikethrough_commands_tip_in_guild = ""] // 使用できないコマンドについての説明を削除
+async fn my_help(
+    ctx: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(ctx, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
